79f26c679f1dbd6aff617e7bb1c8b41409105a8a
biggiesmallz23 <45101587+biggiesmallz23@users.noreply.github.com>
2019-03-20 19:24:28 +0200

    Update sort.py

--- sort.py
+++ sort.py
@@ -14,14 +14,14 @@ for i in range(0, int(elements_number)):                          # Starting a l
 key = len(elements[0])                                            # Assuming the first string of the list as the longest
 
 for i in range(0, len(elements)):                                 # Starting a loop to sort out the longest strings to the new list.
-	if len(elements[i]) > key:                                    # If i-string is longer than the previous one,
-		key = len(elements[i])                                    # it becomes assumed as the longest,
-		largest_elements = []                                     # the list is cleared
-		largest_elements.append(elements[i])                      # and i-string is added to the list of longest ones.
-	elif len(elements[i]) == key:                                 # If i-string's length is equal to the previous one
-		largest_elements.append(elements[i])                      # it's added to the list.
-	else:                                                         # If i-string is shorter than previous one
-		pass                                                      # nothing happens.
+	if len(elements[i]) > key:                                # If i-string is longer than the previous one,
+		key = len(elements[i])                            # it becomes assumed as the longest,
+		largest_elements = []                             # the list is cleared
+		largest_elements.append(elements[i])              # and i-string is added to the list of longest ones.
+	elif len(elements[i]) == key:                             # If i-string's length is equal to the previous one
+		largest_elements.append(elements[i])              # it's added to the list.
+	else:                                                     # If i-string is shorter than previous one
+		pass                                              # nothing happens.
 
 print("The largest elements are: ")                               # Printing the list of the longest strings.
 print(largest_elements)
